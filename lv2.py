daf61e416ebec1d20faf0beb3fb7adbf7f7786de
kbjk00 <kbjk101600@naver.com>
2026-03-22 00:45:39 +0900

    Remove input_info

--- lv2.py
+++ lv2.py
@@ -1,9 +1,6 @@
-input_info = "Pay1oad{g1t_l0g_1s_k3y!}"
-
 user_input = input("Can you find a flag? > ")
 
-if user_input == input_info:
+if user_input == "DELETED":
     print("Correct!")
 else:
     print("Wrong!")
-

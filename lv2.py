f66371edfa74149f819b810fe30cb727cab31177
kbjk00 <kbjk101600@naver.com>
2026-03-22 00:43:56 +0900

    Add input_info

--- lv2.py
+++ lv2.py
@@ -1,3 +1,9 @@
+input_info = "Pay1oad{g1t_l0g_1s_k3y!}"
+
 user_input = input("Can you find a flag? > ")
 
-print("Wrong!")
+if user_input == input_info:
+    print("Correct!")
+else:
+    print("Wrong!")
+
